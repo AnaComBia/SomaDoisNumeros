f12831b912607eff0870e2c2201d683334a8436a
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-11 21:00:48 -0300

    finaliza programa Soma Dois Números

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace SomaDoisNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fale o primeiro número: ");
+            double NumeroUm = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Fale o segundo número: ");
+            double NumeroDois = Convert.ToDouble(Console.ReadLine());
+
+            double Resultado = NumeroUm + NumeroDois;
+
+            Console.WriteLine($"o resultado da soma dos dois números: {NumeroUm} + {NumeroDois} = {Resultado}");
+
+
+        }
+    }
+}
